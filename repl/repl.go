5fbf4d3be587807fec4e9db22daf8d4768fbdb43
Vazanoir <romain.gianolio@proton.me>
2025-01-30 19:33:52 +0100

    add help and exit commands

--- repl/repl.go
+++ repl/repl.go
@@ -11,16 +11,25 @@ func Repl() {
     scanner := bufio.NewScanner(os.Stdin)
 
     for {
-        fmt.Print("Pokedex > ")
+        fmt.Print("\033[31mPokedex > \033[0m")
         scanner.Scan()
-        input := scanner.Text()
-        words := cleanInput(input)
+
+        words := cleanInput(scanner.Text())
         if len(words) < 1 {
             fmt.Println("Please input a command")
             continue
         }
-        command := words[0]
-        fmt.Printf("Your command was: %s\n", command)
+
+        cmd, found := getCommands()[words[0]]
+        if !found {
+            fmt.Println("Unknown command")
+            continue
+        }
+
+        err := cmd.callback()
+        if err != nil {
+            fmt.Printf("error using %s's callback: %v", cmd.name, err)
+        }
     }
 }
 
