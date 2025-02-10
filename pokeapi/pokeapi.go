6bb4f36f56c458af919c48b53931a4d7b9f37d6f
Vazanoir <romain.gianolio@proton.me>
2025-02-10 17:50:13 +0100

    isolate cache use instead of the network request

--- pokeapi/pokeapi.go
+++ pokeapi/pokeapi.go
@@ -22,31 +22,35 @@ type LocationPage struct {
 var c cache.Cache = cache.NewCache(7 * time.Second)
 
 func GetLocations(url string) (LocationPage, error) {
-	body, found := c.Get(url)
+	if body, found := c.Get(url); found {
+        page := LocationPage{}
+        if err := json.Unmarshal(body, &page); err != nil {
+            return LocationPage{}, err
+        }
+        return page, nil
+    }
 
-	if !found {
-		fmt.Printf("INFO: cache not used for '%v'\n", url)
-		res, err := http.Get(url)
-		if err != nil {
-			return LocationPage{}, err
-		}
+    fmt.Printf("INFO: cache not used for '%v'\n", url)
+    res, err := http.Get(url)
+    if err != nil {
+        return LocationPage{}, err
+    }
 
-		body, err = io.ReadAll(res.Body)
-		res.Body.Close()
-		if res.StatusCode > 299 {
-			return LocationPage{}, fmt.Errorf("bad status code: %v", res.StatusCode)
-		}
-		if err != nil {
-			return LocationPage{}, err
-		}
-
-		c.Add(url, body)
-	}
+    body, err := io.ReadAll(res.Body)
+    res.Body.Close()
+    if res.StatusCode > 299 {
+        return LocationPage{}, fmt.Errorf("bad status code: %v", res.StatusCode)
+    }
+    if err != nil {
+        return LocationPage{}, err
+    }
 
 	page := LocationPage{}
 	if err := json.Unmarshal(body, &page); err != nil {
 		return LocationPage{}, err
 	}
+
+	c.Add(url, body)
 
 	return page, nil
 }
