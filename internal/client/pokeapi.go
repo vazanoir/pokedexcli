d755b4475110db8885296e52fa58d32433724e9b
Vazanoir <romain.gianolio@proton.me>
2025-03-04 11:45:42 +0100

    add necessary fields for inspection of a pokemon

--- internal/client/pokeapi.go
+++ internal/client/pokeapi.go
@@ -94,6 +94,23 @@ func (c *Client) ExploreLocations(url string) (ExploreResult, error) {
 type Pokemon struct {
 	BaseExperience int    `json:"base_experience"`
 	Name           string `json:"name"`
+	Weight int `json:"weight"`
+	Height    int `json:"height"`
+	Stats          []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
 }
 
 func (c *Client) CatchPokemon(url string) (Pokemon, error) {
